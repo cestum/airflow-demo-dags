b9c98843f86dc16e83b01b4e2c23312584d5964f
Prem Rajendran <praje@monsanto.com>
2019-06-18 21:49:51 -0500

    update dag

--- dags/3steps.py
+++ dags/3steps.py
@@ -17,57 +17,53 @@ default_args = {
 dag = DAG(
     '3 Step Model flow', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
-volume_mount = VolumeMount('data-volume',
-                           mount_path='/',
+input_volume_mount = VolumeMount('data-volume',
+                           mount_path='/input-dataset',
+                           sub_path=None,
+                           read_only=False)
+
+output_volume_mount = VolumeMount('data-volume',
+                           mount_path='/output-dataset',
                            sub_path=None,
                            read_only=False)
 volume_config= {
     'persistentVolumeClaim':
     {
-        'claimName': 'airflow-dag' # uses the persistentVolumeClaim given in the Kube yaml
+        'claimName': 'airflow-dags' # uses the persistentVolumeClaim given in the Kube yaml
     }
 }
 
-volume = Volume(name='data-volume', configs=volume_config)
+in_volume = Volume(name='input-dataset', configs=volume_config)
+out_volume = Volume(name='output-dataset', configs=volume_config)
 
 step1 = KubernetesPodOperator(namespace='airflow',
-                          image="python:3.6",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
+                          image="cestum/airflow-demo:prep-input",
+                          cmds=[],
+                          arguments=["K8S-Airflow"],
                           labels={"foo": "bar"},
-                          name="passing-test",
-                          volumes=[volume],
-                          volume_mounts=[volume_mount]
-                          task_id="passing-task",
+                          name="prep-input",
+                          volumes=[in_volume],
+                          volume_mounts=[input_volume_mount]
+                          task_id="prep-input",
                           get_logs=True,
                           dag=dag,
                           in_cluster=True
                           )
 
 step2 = KubernetesPodOperator(namespace='airflow',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
+                          image="cestum/airflow-demo:model-run",
+                          cmds=[],
+                          arguments=["3"],
                           labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
+                          name="model-run",
+                          volumes=[out_volume],
+                          volume_mounts=[output_volume_mount]                          
+                          task_id="model-run",
                           get_logs=True,
                           dag=dag,
                           in_cluster=True
                           )
 
-step3 = KubernetesPodOperator(namespace='airflow',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag,
-                          in_cluster=True
-                          )
 
 step2.set_upstream(step1)
-step3.set_upstream(step2)
 
